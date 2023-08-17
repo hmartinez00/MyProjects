334ea39b05abbbfa7ef1bc2d20657c99840d682e
hmartinez <hmartinez@abae.gob.ve>
2023-08-17 19:09:21 -0400

    Version Estable 20230817 190920

--- module/w_tools.py
+++ module/w_tools.py
@@ -64,3 +64,50 @@ def talk(lectura):
     engine.setProperty('voice', voices[0].id)
     engine.say(lectura)
     engine.runAndWait()
+
+
+class orders:
+
+    def __init__(
+        self,
+        file,
+        __dictado__
+    ):
+
+        self.file = file
+        self.__dictado__ = __dictado__
+
+        string = ''
+        with open(self.file, encoding='utf-8') as f:
+            for line in f:
+                string = string + line
+        
+        self.string = string
+
+    def close_options(self, __list__):
+        if self.__dictado__ in __list__:        
+            return True
+
+    def secuence_options(self, __list__A, __list__B):
+       
+        if self.__dictado__ in __list__A:        
+            for i in range(len(__list__A)):
+                if self.__dictado__ == __list__A[i]:
+                    j = i
+
+            string = self.string + __list__B[j]
+       
+        else:
+            string = self.string + ' ' + str(self.__dictado__)
+        
+        with open(self.file, 'w', encoding='utf-8') as f:
+            f.write(string)
+        f.close()
+
+    def clear(self, __list__):
+        if self.__dictado__ in __list__:
+            string = ''
+            with open(self.file, 'w', encoding='utf-8') as f:
+                f.write(string)
+            f.close()
+
--- scripts/test.py
+++ scripts/test.py
@@ -1,7 +1,11 @@
+from Eliezer.speech_recognizer
 from module.w_tools import Reconocimiento
 
-ans = Reconocimiento(1)
-print(ans)
+while True:
+    ans = Reconocimiento(1)
+    if ans != ''
+    print(ans)
+
 
 # url = 'https://www.biblegateway.com/passage/?search=proverbios+3%3A5-6&version=RVR1960'
 # tags = raspado(url)
