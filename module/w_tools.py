aa1ae90d8c8e56405897fa5145c0d11cf4e8dfff
hmartinez <hmartinez@abae.gob.ve>
2023-08-17 16:19:15 -0400

    Version Estable 20230817 161914

--- module/w_tools.py
+++ module/w_tools.py
@@ -1,10 +1,12 @@
+import pyaudio
 import requests
 from bs4 import BeautifulSoup
 import speech_recognition as sr
 
 def Reconocimiento():
     r = sr.Recognizer()
-    with sr.Microphone() as source:
+    # with sr.Microphone() as source:
+    with sr.WasapiLoopbackDevice() as source:
         print('Te escucho: ')
         r.adjust_for_ambient_noise(source)
         audio = r.listen(source)
@@ -22,3 +24,19 @@ def raspado(url):
     links = bs.find_all('p')
     
     return links
+
+def obtener_microfonos_disponibles():
+    p = pyaudio.PyAudio()
+    info = p.get_host_api_info_by_index(0)
+    num_devices = info.get('deviceCount')
+    microfonos = []
+    for i in range(0, num_devices):
+        device_info = p.get_device_info_by_host_api_device_index(0, i)
+        if device_info.get('maxInputChannels') > 0:
+            microfono = {
+                'indice': i,
+                'nombre': device_info.get('name')
+            }
+            microfonos.append(microfono)
+    p.terminate()
+    return microfonos
--- scripts/test.py
+++ scripts/test.py
@@ -1,8 +1,13 @@
-from module.w_tools import Reconocimiento, raspado
+from module.w_tools import Reconocimiento, raspado, obtener_microfonos_disponibles
 
 # ans = Reconocimiento()
 # print(ans)
 
-url = 'https://www.biblegateway.com/passage/?search=proverbios+3%3A5-6&version=RVR1960'
-tags = raspado(url)
-print(tags)
+# url = 'https://www.biblegateway.com/passage/?search=proverbios+3%3A5-6&version=RVR1960'
+# tags = raspado(url)
+# print(tags)
+
+ # Ejemplo de uso
+microfonos = obtener_microfonos_disponibles()
+for microfono in microfonos:
+    print(f"Indice: {microfono['indice']}, Nombre: {microfono['nombre']}")
