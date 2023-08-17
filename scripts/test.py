fbeeefb470054c665d922f16fcbdb9de25118d3a
hmartinez <hmartinez@abae.gob.ve>
2023-08-17 19:34:32 -0400

    Version Estable 20230817 193432

--- scripts/test.py
+++ scripts/test.py
@@ -1,10 +1,9 @@
-from Eliezer.speech_recognizer
-from module.w_tools import Reconocimiento
+from General_Utilities.control_rutas import setting_routes
+from module.w_tools import recognizer
 
-while True:
-    ans = Reconocimiento(1)
-    if ans != ''
-    print(ans)
+key = 'voice'
+ruta_archivo_json = setting_routes(key)[0]
+recognizer(ruta_archivo_json)
 
 
 # url = 'https://www.biblegateway.com/passage/?search=proverbios+3%3A5-6&version=RVR1960'
