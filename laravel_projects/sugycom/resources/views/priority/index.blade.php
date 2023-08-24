bf89dff8f745e84a54933ceebb6ebc17941db940
hmartinez <hmartinez@abae.gob.ve>
2023-08-24 18:49:02 -0400

    Version Estable 20230824 184858

--- laravel_projects/sugycom/resources/views/priority/index.blade.php
+++ laravel_projects/sugycom/resources/views/priority/index.blade.php
@@ -1,10 +1,30 @@
 @extends('layouts.app')
 @section('content')
-    <ul>
-        @forelse ($priorities as $priority)
-            <li>{{ $priority->id }}</li>
-        @empty
-            <p>No data.</p>
-        @endforelse
-    </ul>
+    <h2>Priorities</h2>
+    <div class="table-responsive small">
+        <table class="table table-striped table-sm">
+            <thead>
+                <tr>
+                    <th scope="col">id</th>
+                    <th scope="col">satellite</th>
+                    <th scope="col">target</th>
+                    <th scope="col">status</th>
+                    <th scope="col">created_at</th>
+                </tr>
+            </thead>
+            <tbody>
+            @forelse ($priorities as $priority)
+                <tr>
+                    <td>{{ $priority->id }}</td>
+                    <td>{{ $priority->satellite }}</td>
+                    <td>{{ $priority->target }}</td>
+                    <td>{{ $priority->status }}</td>
+                    <td>{{ $priority->created_at }}</td>
+                </tr>
+                @empty
+                <p>No data.</p>
+                @endforelse
+            </tbody>
+        </table>
+    </div>
 @endsection
