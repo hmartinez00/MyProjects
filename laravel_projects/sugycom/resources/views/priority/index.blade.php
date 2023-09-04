14cabc41e1435e543abe324eb240959f6903b6bf
hmartinez <hmartinez@abae.gob.ve>
2023-09-04 17:45:21 -0400

    Version Estable 20230904 174521

--- laravel_projects/sugycom/resources/views/priority/index.blade.php
+++ laravel_projects/sugycom/resources/views/priority/index.blade.php
@@ -1,10 +1,6 @@
 @extends('layouts.app')
 @section('content')
     <div class="content">
-        <button class="btn btn-primary">
-            <a href="{{ route('priority.create') }}"><i class="fa fa-plus"></i></a>
-        </button>
-        <h2>Priorities</h2>
         <table class="table table-sm table-dark table-hover">
             <thead>
                 <tr>
@@ -18,37 +14,39 @@
             <tbody class="table-group-divider">
                 @forelse ($priorities as $priority)
                     <tr>
-                        <td><a class="nav-link text-white" data-bs-toggle="offcanvas" href="#offcanvasExample">{{ $priority->id }}</a></td>
-                        <td><a class="nav-link text-white" href="{{ route('priority.show', $priority->id) }}">{{ $priority->satellite }}</a></td>
-                        <td><a class="nav-link text-white" href="{{ route('priority.show', $priority->id) }}"><span class="d-inline-block text-truncate" style="max-width: 150px">{{ $priority->target }}</span></a></td>
-                        <td><a class="nav-link text-white" href="{{ route('priority.show', $priority->id) }}">{{ $priority->status }}</a></td>
-                        <td><a class="nav-link text-white" href="{{ route('priority.show', $priority->id) }}">{{ $priority->created_at }}</a></td>
+                        <td><a class="nav-link text-white" data-bs-toggle="offcanvas" href="#{{ $priority->id }}">{{ $priority->id }}</a></td>
+                        <td><a class="nav-link text-white" data-bs-toggle="offcanvas" href="#{{ $priority->id }}">{{ $priority->satellite }}</a></td>
+                        <td><a class="nav-link text-white" data-bs-toggle="offcanvas" href="#{{ $priority->id }}"><span class="d-inline-block text-truncate" style="max-width: 150px">{{ $priority->target }}</span></a></td>
+                        <td><a class="nav-link text-white" data-bs-toggle="offcanvas" href="#{{ $priority->id }}">{{ $priority->status }}</a></td>
+                        <td><a class="nav-link text-white" data-bs-toggle="offcanvas" href="#{{ $priority->id }}">{{ $priority->created_at }}</a></td>
                     </tr>
+
+                    <div class="offcanvas offcanvas-start" tabindex="-1" id="{{ $priority->id }}">
+                        <div class="offcanvas-header">
+                            <h5 class="offcanvas-title" id="offcanvasExampleLabel">Menú</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                        </div>
+                        <div class="offcanvas-body">
+                            <div>
+                                Acá puede elegir desplegar las caracteristicas del item seleccionado, borrarlo, o crear un nuevo item.
+                            </div>
+                            <div class="dropdown mt-3">
+                                <button class="btn btn-primary dropdown-toggle" type="button" data-bs-toggle="dropdown">
+                                    Dropdown Button
+                                </button>
+                              <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="{{ route('priority.show', $priority->id) }}">Mostrar item {{ $priority->id }}</a></li>
+                                <li><a class="dropdown-item" href="{{ route('priority.destroy', $priority->id) }}">Borrar item {{ $priority->id }}</a></li>
+                                <li><a class="dropdown-item" href="{{ route('priority.create') }}">Crear nuevo item</a></li>
+                              </ul>
+                            </div>
+                        </div>
+                    </div>
+
                 @empty
                     <p>No data.</p>
                 @endforelse
             </tbody>
         </table>
-    </div>
-    <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample">
-        <div class="offcanvas-header">
-            <h5 class="offcanvas-title" id="offcanvasExampleLabel">Offcanvas</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-        </div>
-        <div class="offcanvas-body">
-            <div>
-              Some text as placeholder. In real life you can have the elements you have chosen. Like, text, images, lists, etc.
-            </div>
-            <div class="dropdown mt-3">
-              <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown">
-                Dropdown button
-              </button>
-              <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">Action</a></li>
-                <li><a class="dropdown-item" href="#">Another action</a></li>
-                <li><a class="dropdown-item" href="#">Something else here</a></li>
-              </ul>
-            </div>
-        </div>
     </div>
 @endsection
