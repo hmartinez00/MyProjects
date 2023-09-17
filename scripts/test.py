f740a7acc6cedd9d5bfa0820e733c06333a8fedd
hmartinez <hmartinez@abae.gob.ve>
2023-09-17 13:52:08 -0400

    Version Estable 20230917 135208

--- scripts/test.py
+++ scripts/test.py
@@ -1,8 +1,61 @@
 import pandas as pd
+import numpy as np
 from ManageDB.sqlite_on_db import selectall
+from ManageDB.mysql_on_db import Insertar_registro_masivo
 
+
+def extraer_cabeceras(__base_datos__, __tabla__):
+    import mysql.connector
+    # Establecer la conexión a la base de datos
+    conexion = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database=__base_datos__
+    )
+    # Crear un cursor para ejecutar consultas
+    cursor = conexion.cursor()
+    # Definir el nombre de la tabla
+    nombre_tabla = __tabla__
+    # Consulta para obtener las cabeceras de la tabla
+    consulta = f"SHOW COLUMNS FROM {nombre_tabla}"
+    # Ejecutar la consulta
+    cursor.execute(consulta)
+    # Obtener los resultados
+    resultados = cursor.fetchall()
+    # Imprimir las cabeceras
+    fields = []
+    for resultado in resultados:
+        fields.append(resultado[0])
+    # Cerrar el cursor y la conexión a la base de datos
+    cursor.close()
+    conexion.close()
+
+    return fields
+
+
+# Extraemos los datos de la tabla de partida
 db = r'C:\Users\admin\Documents\0 - A Repositorios GitHub\Devocionales\2tim4_1.db'
 table = 'plan_biblia_52'
 df = selectall(db, table)
 
+for i in range(2):
+    tag_col = f'column{i}'
+    df[tag_col] = pd.to_datetime(['' for _ in range(len(df))], format='%m/%d/%Y %H:%M')
+    df[tag_col] = df[tag_col].dt.strftime('%Y-%m-%d %H:%M:%S')
+
+
+# Extraemos las columnas de la tabla de llegada
+base_datos = 'devocionales'
+tabla = 'plan_biblia_52s'
+cabeceras = extraer_cabeceras(base_datos, tabla)
+df.columns = cabeceras
+
+print(cabeceras)
+df = df.set_index('id')
 print(df)
+
+# Insertar en Database
+Insertar_registro_masivo(df, base_datos, tabla)
+
+
