9c44d2a36ba7708a4c1ebef1a7304c1d6daf46be
hmartinez <hmartinez@abae.gob.ve>
2023-09-23 12:33:12 -0400

    Version Estable 20230923 123312

--- laravel_projects/devocionales/resources/views/plan_biblia_52/show.blade.php
+++ laravel_projects/devocionales/resources/views/plan_biblia_52/show.blade.php
@@ -1,23 +1,58 @@
 @extends('layouts.own.app')
 @section('content')
     <div class="content">
-        <table class="table-show">
-            <tbody>
-                @foreach ($headers as $header)
-                    <tr>
-                        <td>{{ $header }}:</td>
-                        <td>{{ $plan_biblia_52->$header }}</td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
-        <form action="{{ route('plan_biblia_52.destroy', $plan_biblia_52->id) }}" method="post">
-            @csrf
-            @method('DELETE')
-            <button type="submit" class="btn-danger">Delete</button>
-            <button class="btn-primary">
-                <a href="{{ route('plan_biblia_52.edit', $plan_biblia_52->id) }}">Edit</a>
-            </button>
-        </form>
+        <div class="row">
+            <div class="col-6">
+                <table class="table-show">
+                    <tbody>
+                        @foreach ($headers as $header)
+                        <tr>
+                            <td>{{ $header }}:</td>
+                            <td>{{ $plan_biblia_52->$header }}</td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+            <div class="col-6">
+
+                <ul class="dropdown-menu position-static d-grid gap-1 p-2 rounded-3 mx-0 border-0 shadow w-220px" data-bs-theme="dark">
+                    <li>
+                        {{-- <a class="dropdown-item rounded-2 active" href="{{ route('plan_biblia_52.edit', $plan_biblia_52->id) }}"> --}}
+                        <a class="dropdown-item rounded-2" href="{{ route('plan_biblia_52.edit', $plan_biblia_52->id) }}">
+                            <font style="vertical-align: inherit;">
+                                <font style="vertical-align: inherit;">Edit</font>
+                            </font>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="dropdown-item rounded-2" href="#">
+                            <font style="vertical-align: inherit;">
+                                <font style="vertical-align: inherit;">Send by Telegram</font>
+                            </font>
+                        </a>
+                    </li>
+                    <li>
+                        <hr class="dropdown-divider">
+                    </li>
+                    <li>
+                        {{-- <a class="dropdown-item rounded-2" href="{{ route('plan_biblia_52.destroy', $plan_biblia_52->id) }}"> --}}
+                            <form action="{{ route('plan_biblia_52.destroy', $plan_biblia_52->id) }}" method="post">
+                                @csrf
+                                @method('DELETE')
+                                {{-- <button type="submit" class="btn-danger"> --}}
+                                    {{-- <font style="vertical-align: inherit;"> --}}
+                                        {{-- <font style="vertical-align: inherit;">Delete</font> --}}
+                                    {{-- </font>     --}}
+                                {{-- </button> --}}
+                                <input type="submit" value="Delete">
+                            </form>
+                            
+                        {{-- </a> --}}
+                    </li>
+                </ul>
+
+            </div>
+        </div>
     </div>
 @endsection
