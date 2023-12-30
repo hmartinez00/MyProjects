587dffa513e1bd1e79cd95c50861c79a80166501
hmartinez <hmartinez@abae.gob.ve>
2023-12-30 19:23:18 -0400

    Version Estable 20231230 192318

--- laravel_projects/sugycom/resources/views/trigger/index.blade.php
+++ laravel_projects/sugycom/resources/views/trigger/index.blade.php
@@ -44,9 +44,33 @@
 
         <table class="table table-sm table-dark table-hover">
             <tbody>
-                @forelse ($directories as $directory)
-                    @if ( $directory !== '.' && $directory !== '..' && $directory !== 'data' && $directory !== 'backup' && strpos($directory, '.zip') === false )
-                        <tr><td>{{ $directory }}</td></tr>
+                @forelse ($directories as $data_item)
+                    @if ( $data_item !== '.' && $data_item !== '..' && $data_item !== 'data' && $data_item !== 'backup' && strpos($data_item, '.zip') === false )
+                        {{-- <tr><td>{{ $data_item }}</td></tr> --}}
+                        <tr><td><a class="nav-link text-white" data-bs-toggle="offcanvas" href="#{{ $data_item }}">{{ $data_item }}</a></td></tr>
+                        
+                        <div class="offcanvas offcanvas-start" tabindex="-1" id="{{ $data_item }}">
+                            <div class="offcanvas-header">
+                                <h5 class="offcanvas-title" id="offcanvasExampleLabel">Menú</h5>
+                                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                            </div>
+                            <div class="offcanvas-body">
+                                <div>
+                                    Acá puede elegir desplegar las caracteristicas del item seleccionado, actualizarlo o borrarlo.
+                                </div>
+                                <div class="dropdown mt-3">
+                                    <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown">
+                                        Dropdown Button
+                                    </button>
+                                  <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="#">Leer item {{ $data_item }}</a></li>
+                                    <li><a class="dropdown-item" href="#">Actualizar item {{ $data_item }}</a></li>
+                                  </ul>
+                                </div>
+                            </div>
+                        </div>
+
+
                     @endif
                 @empty
                     <tr><td>No data.</td></tr>
