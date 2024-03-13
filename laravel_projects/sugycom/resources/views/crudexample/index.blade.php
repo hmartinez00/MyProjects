994df538348425a520cb28307755aadb0cf9930b
hmartinez <hmartinez@abae.gob.ve>
2024-03-13 19:45:28 -0400

    Version Estable 20240313 194526

--- laravel_projects/sugycom/resources/views/crudexample/index.blade.php
+++ laravel_projects/sugycom/resources/views/crudexample/index.blade.php
@@ -55,13 +55,12 @@
                                     <td><a class="nav-link text-white" data-bs-toggle="offcanvas" href="#{{ $data_item->id }}">{{ $data_item->$s_header }}</a></td>
                                 @endforeach
                                 <td>
-                                    <form method="POST" action="{{ route($views_category . '.destroy', [$views_category => $data_item->id]) }}">
+                                    <form method="GET" action="{{ route($views_category . '.show', [$views_category => $data_item->id]) }}">
                                         @csrf
-                                        @method('DELETE')
-                                        <button type="submit" class="btn btn-outline-danger">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-                                                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
-                                                <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+                                        <button type="submit" class="btn btn-outline-info">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
+                                                <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z"/>
+                                                <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0"/>
                                             </svg>
                                         </button>
                                     </form>
@@ -70,20 +69,30 @@
 
                             <div class="offcanvas offcanvas-start" tabindex="-1" id="{{ $data_item->id }}">
                                 <div class="offcanvas-header">
-                                    <h5 class="offcanvas-title" id="offcanvasExampleLabel">Menú</h5>
+                                    <h5 class="offcanvas-title" id="offcanvasExampleLabel">Special options</h5>
                                     <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                                 </div>
                                 <div class="offcanvas-body">
                                     <div>
-                                        Acá puede elegir desplegar las caracteristicas del item seleccionado, actualizarlo o borrarlo.
-                                    </div>
+                                        Here you can choose to display the special characteristics of the selected item.</div>
                                     <div class="dropdown mt-3">
                                         <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown">
                                             Dropdown Button
                                         </button>
                                     <ul class="dropdown-menu">
-                                        <li><a class="dropdown-item" href="{{ route($views_category . '.show', [$views_category => $data_item->id]) }}">Leer item {{ $data_item->id }}</a></li>
-                                        <li><a class="dropdown-item" href="{{ route($views_category . '.edit', [$views_category => $data_item->id]) }}">Actualizar item {{ $data_item->id }}</a></li>
+                                        <li>
+                                            <form method="POST" action="{{ route($views_category . '.destroy', [$views_category => $data_item->id]) }}">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="btn btn-outline-danger m-2 mt-3">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                                                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+                                                        <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+                                                    </svg>
+                                                    Borrar item {{ $data_item->id }}
+                                                </button>
+                                            </form>
+                                        </li>
                                     </ul>
                                     </div>
                                 </div>
